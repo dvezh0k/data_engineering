d998dc9616adbe62279291d64556fc2263661fb0
dvezh0k <dzezhov01@gmail.com>
2026-04-17 12:51:29 +0300

    fix: create users updated_at trigger

--- src/databases/postgresql/task.sql
+++ src/databases/postgresql/task.sql
@@ -17,6 +17,25 @@ CREATE TABLE users_audit (
     new_value TEXT
 );
 
+/* Создание функции обновления поля updated_at в таблице users */
+create or replace function replace_updated_at()
+returns trigger as $$
+begin
+	update users
+	set updated_at = now()
+	where id = new.id;
+	
+	return new;
+end;
+$$ language plpgsql;
+
+/* Создание триггера на обновление поля updated_at после обновления записей в users */
+create or replace trigger trigger_replace_updated_at
+after update on users
+for each row
+when (old.updated_at is not distinct from new.updated_at)
+execute function replace_updated_at();
+
 /* Создание функции логирования изменений */
 create or replace function log_user_info_update()
 returns trigger as $$
@@ -29,18 +48,18 @@ begin
 	select key, value::text from jsonb_each_text(to_jsonb(new))
 	loop
 		old_value := (to_jsonb(old) ->> field_name)::text;
-		if new_value is distinct from old_value then
+		if new_value is distinct from old_value and field_name <> 'updated_at' then
 			insert into users_audit(user_id, changed_by, field_changed, old_value, new_value)
 			values (old.id, current_user, field_name, old_value, new_value);
 		end if;
 	end loop;
 
-	return new;changed_by
+	return new;
 end;
 $$ language plpgsql;
 
 /* Создание триггера на функцию логирования изменений */
-create trigger trigger_log_user_info_update
+create or replace trigger trigger_log_user_info_update
 before update on users
 for each row
 execute function log_user_info_update();
